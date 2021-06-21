b310c17d9fa356a853a53f30f16874d9b5e4dded
Luna0214 <54482832+Luna0214@users.noreply.github.com>
2021-06-21 15:44:46 +0900

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,46 @@
+# #조건1. 유튜버들을 리스트형태로 2명이상 불러오시오.
+# name1 = "윤조"
+# name2 = "회진"
+# name3 = "영구"
+# name4 = "민섭"
+
+# names = []
+
+# names.append(name1)
+# names.append(name2)
+# names.append(name3)
+# names.append(name4)
+
+# # print(names)
+
+
+# #조건2. (유튜브이름).txt 파일을 각각 이름에 맞게 구성하시오.
+# x = 0
+# i = names[x]
+
+
+# for i in names:
+#     with open("유튜버" + str(i) + ".txt", "w", encoding="utf8") as email_file:
+#         email_file.write("안녕하세요?" + i + "님.")
+#         email_file.write("\n(주)나도출판 편집자 나코입니다.")
+#         email_file.write("\n현재 저희 출판사는 파이썬에 관한 주제로 책 출간을 기획 중입니다.\n")
+#         email_file.write( i + "님의 유튜브 영상을 보고 연락을 드리게 되었습니다.")
+#         email_file.write("\n자세한 내용은 첨부드리는 제안서를 확인 부탁드리며, 긍정적인 회신 기다리겠습니다.\n")
+#         email_file.write("\n")
+#         email_file.write("좋은 하루 보내세요 ^^\n")
+#         email_file.write("감사합니다.\n")
+#         email_file.write("- 나코 드림.\n")
+#         email_file.close()
+#     x =+1
+
+#----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+    
